b38812abdc879354e0c289f3ca361c7e36407e3b
imConfused3684 <oleggrirorev@yandex.ru>
2024-04-09 20:53:14 +0400

    rethinking the server users structure

--- server/src/pages/Users/UsersCtrl.ts
+++ server/src/pages/Users/UsersCtrl.ts
@@ -1,23 +1,15 @@
 import { Router } from 'express';
 import { UsersR as R } from './UsersR';
-import { UsersM } from './Model/UsersM';
+import { UsersSQL } from "../../Infrastructure/Repository/UsersSQL";
 
 const router = Router();
 
-export class UsersCtrl {
-    public UsersM: UsersM
-
-    constructor() {
-        this.UsersM = new UsersM();
-    }
-}
 
 router.post(R.loginCheck.route, async (req, res) => {
 
     const request = req.body as R.loginCheck.RequestI;
 
-    const ctrl = new UsersCtrl();
-    const response = await ctrl.UsersM.loginCheck(request);
+    const response = await (new UsersSQL).loginCheck(request.login, request.password);
 
     res.json(response)
 });
