a261f5258d0ff5747fedda65b316387b99229355
imConfused3684 <oleggrirorev@yandex.ru>
2024-04-20 14:43:56 +0400

    update: footer minor chages

--- client/src/common/el/footer/footer.tsx
+++ client/src/common/el/footer/footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
     return (
         <div className={styles.outerWrapper}>
             <div className={styles.innerWrapper}>
-               <div>
+               <div className={styles.footerhalf}>
                     Контакты<br />
                     +7(232)964-23-12 contact@ya.ru<br />
                     Адрес<br />
                     Ул. Мира, Дом Кефира
                </div>
-               <div>
+               <div className={styles.footerhalf}>
                     <NavLink to="/">Главная</NavLink><br />
                     <NavLink to="/catalogue">Каталог</NavLink><br />
                     <NavLink to="/favourite">Избранное</NavLink><br />
