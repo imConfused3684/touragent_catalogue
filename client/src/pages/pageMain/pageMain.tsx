5063234d9f2c755255a257d0a8f4262e6be91220
imConfused3684 <oleggrirorev@yandex.ru>
2024-04-23 23:44:58 +0400

    testing before after

---
+++ client/src/pages/pageMain/pageMain.tsx
@@ -0,0 +1,14 @@
+import styles from "./pageMain.module.css";
+import { NavLink } from "react-router-dom";
+
+export default function Main() {
+    return (
+        <>
+            <h1>HI</h1>
+
+            <div className={styles.test}>
+                <p>asdasd</p>
+            </div>
+        </>
+    );
+}
