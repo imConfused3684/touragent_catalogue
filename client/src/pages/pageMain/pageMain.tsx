f44b095d4bccf85dd72d91ffcfe3787c2d358d88
imConfused3684 <oleggrirorev@yandex.ru>
2024-04-25 18:27:44 +0400

    update: some main page progress

--- client/src/pages/pageMain/pageMain.tsx
+++ client/src/pages/pageMain/pageMain.tsx
@@ -1,14 +1,35 @@
 import styles from "./pageMain.module.css";
 import { NavLink } from "react-router-dom";
+import mainOcean from "../../assets/mainOcean.jpg"
+import mainTurtle from "../../assets/mainTurtle.png"
+import pointimg from "../../assets/checkblue.png"
 
 export default function Main() {
     return (
-        <>
-            <h1>HI</h1>
+        <div className={styles.wrapper}>
+            <h1>Traveling Around</h1>
 
-            <div className={styles.test}>
-                <p>asdasd</p>
+            <div className={styles.adimg}>
+                <img src={mainTurtle} alt="mainImage" />
             </div>
-        </>
+
+            <div className={styles.points}>
+                <div className={styles.point}>
+                    <img src={pointimg}/>
+                    <p>Предоставляем эксклюзивные <br/>предложения по доступным ценам</p>
+                </div>
+
+                <div className={styles.point}>
+                    <img src={pointimg}/>
+                    <p>Заботимся о вашем комфорте <br/>и безопасности</p>
+                </div>
+
+                <div className={styles.point}>
+                    <img src={pointimg}/>
+                    <p>Опытные специалисты помогут вам <br/>забронировать идеальное место</p>
+                </div>
+            </div>
+
+        </div>
     );
 }
