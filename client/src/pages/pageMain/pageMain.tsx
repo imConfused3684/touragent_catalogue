7885db4a16a3f80179351e25bfc8ad2bc03995ab
imConfused3684 <oleggrirorev@yandex.ru>
2024-04-26 15:49:58 +0400

    update: main page done

--- client/src/pages/pageMain/pageMain.tsx
+++ client/src/pages/pageMain/pageMain.tsx
@@ -1,24 +1,38 @@
 import styles from "./pageMain.module.css";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import Button from "../../common/el/commonButton/combut"
+
 import mainOcean from "../../assets/mainOcean.jpg"
+import mainRocks from "../../assets/mainRocks.jpg"
+import mainPalms from "../../assets/mainPalms.jpg"
 import mainTurtle from "../../assets/mainTurtle.png"
 import pointimg from "../../assets/checkblue.png"
 
+
+
 export default function Main() {
+    const [sliderIndex, setsliderIndex] = useState<number>(0);
+
+    useEffect(()=>{
+        setInterval(() => {
+            setsliderIndex((sliderIndex + 1)%4);
+          }, 4000);
+    },[sliderIndex])
+
     return (
         <div className={styles.wrapper}>
             <h1>Traveling Around</h1>
 
             <div className={styles.adimg}>
-                <img src={mainTurtle} alt="mainImage" />
+                <img src={sliderIndex == 0 ? mainTurtle : ""} alt="mainImage" />
+                <img src={sliderIndex == 1 ? mainPalms : ""} alt="mainImage" />
+                <img src={sliderIndex == 2 ? mainRocks : ""} alt="mainImage" />
+                <img src={sliderIndex == 3 ? mainOcean : ""} alt="mainImage" />
             </div>
 
             <div className={styles.points}>
-                <div className={styles.point}>
-                    <img src={pointimg}/>
-                    <p>Предоставляем эксклюзивные <br/>предложения по доступным ценам</p>
-                </div>
-
                 <div className={styles.point}>
                     <img src={pointimg}/>
                     <p>Заботимся о вашем комфорте <br/>и безопасности</p>
@@ -28,8 +42,16 @@ export default function Main() {
                     <img src={pointimg}/>
                     <p>Опытные специалисты помогут вам <br/>забронировать идеальное место</p>
                 </div>
+
+                <div className={styles.point}>
+                    <img src={pointimg}/>
+                    <p>Предоставляем эксклюзивные <br/>предложения по доступным ценам</p>
+                </div>
             </div>
 
+            <NavLink to="/catalogue" className={styles.bottomButtonWrapper}>
+                <Button text="Начать поиски!" func={()=>{}} />
+            </NavLink>
         </div>
     );
 }
